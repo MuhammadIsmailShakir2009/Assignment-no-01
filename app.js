021c36da413057d3455ac74f35464e6c30d945ad
Muhammad Ismail Shakir <160407658+MuhammadIsmailShakir2009@users.noreply.github.com>
2024-06-04 20:56:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+// Q1
+//                     BioData Details"
+let h1 = "BioData Details"
+let myName = "Muhammad Ismail Jetal"
+let myAge = "15"
+let occupation = "web desginer"
+let emai = "mismail@gmail.com"
+let phoneNumber = "+9203222376492"
+let instituteName = "Jawan Pakistan"
+let education = "Under martric"
+document.write(`<center><u>${h1} :</center></u>  <br> Name:  ${myName} <br>  Age:  ${myAge} <br>  Occupation:  ${occupation} <br>  Email:  ${emai} <br>  PhoneNumber:  ${phoneNumber} <br>  InstituteName: 
+ ${instituteName} <br>  Education:  ${education} <br>`);
+
+// Q2
+let haeding1=" Mark Sheet"
+let studentName = "Muhammad Zaid"
+let studentFatherName = "Usman"
+let studentAge = "20"
+let StudentInstituteName = "Jawan Pakistan"
+let studentSubject1 = "Computer"
+let studentSubject2 = "Chemistry"
+let studentSubject3 = "physics"
+let subjectObtainedNumber = "250"
+let subjectTotalNumber = "300"
+let ObtainedCalculation = "250/300*100"
+let percentage = "83.3%"
+let grade = "A++"
+document.write(`<center><u>${haeding1}</u></center>StudentName: ${studentName} <br> FatherName: ${studentFatherName} <br> StudentAge: ${studentAge} <br> InstituteName: ${StudentInstituteName}
+<br> StudentObtained Marks: ${subjectObtainedNumber} <br> TotalNumber: ${subjectTotalNumber} <br> Percentage: ${percentage}
+<br> PassingGrade: ${grade}`)
+// Q3
+// var firstName ="muhammad"
+// {
+//     firstName="ismail"
+//     console.log(firstName)
+// }
+// console.log(firstName)
+// Let Keyword
+// let firstName ="muhammad"
+// {
+//     let firstName="ismail"
+//     console.log(firstName)
+// }
+// console.log(firstName)
+// const Keyword    
+// const firstName ="muhammad"
+// {
+//     const firstName="ismail"
+//     console.log(firstName)
+// }
+// console.log(firstName)
+// function foo() {
+//     var a ="20";
+//     console.log(a)
+// }
+// foo()
+// const obj={
+//     name: "ismail",
+//     emai: "ismail@gmail.com",
+//     city:  "karachi",
+// }
+// obj.city="lahore"
+// console.log(obj)
+// var age="20"
+// var age="25"
+// console.log(age)
+// var obj={
+//     name: "ismail",
+//     emai: "ismail@gmail.com",
+//     city:  "karachi",
+// }
+// obj.city="lahore"
+// console.log(obj)
+// let obj={
+//     name: "ismail",
+//     emai: "ismail@gmail.com",
+//     city:  "karachi",
+// }
+// obj.city="lahore"
+// console.log(obj)
+// console.log(name)
+// var name = "ismail"
+// console.log(firstName)
+// let firstName = "ismail"
+// console.log(firstName)
+// Template literals
+// let firstName="ismail"
+// let lastName="jeatl"
+// let age="20"
+// let bioData="hellow" + firstName + lastName + ". your age is " + age
+// let bioData=`hellow first name is ${firstName} ${lastName} your age is ${age}`
+// console.log(bioData)
+
+                    //  FOR MULTIPILICTION TABLE BY TEMPELATE LITERALS
+// let user = +prompt("Enter your number");
+// let userRange = +prompt("Enter table range");
+
+// for(let i = 1; i <= userRange; i++) {
+//     document.write(`${user} x ${i} = ${user * i} <br>`);
+// }
+
+let end = "================= THE END ================="
+document.write(`<br> <br> <br> <center>${end}</center> <br> <br>`)
